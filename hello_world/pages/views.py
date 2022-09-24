85c8aafef641873fafd097c27e068bdd805500ed
ankita130-byte <ankita00009999311095@gmail.com>
2022-09-24 12:23:31 +0530

    just added a new code

--- hello_world/pages/views.py
+++ hello_world/pages/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def homePageView(request):
-    return HttpResponse("Hello, world!")
+    return HttpResponse("Hello, world!, This is my first Git")
 
 # Create your views here.
